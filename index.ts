07228d47e3d69cf4fee3a3c21d24363c130b3e52
jagi113 <jaroslavgirovsky@gmail.com>
2022-10-20 12:36:55 +0200

    all done

--- index.ts
+++ index.ts
@@ -14,9 +14,13 @@ axios.get("https://jsonplaceholder.typicode.com/todos/1").then(response => {
     const title = todo.title;
     const finished = todo.completed;
 
+    logTodo(ID, title, finished);
+});
+
+const logTodo = (id: number, title:string, finished: boolean) => {
     console.log(`
-        The TODO with ID: ${ID}
+        The TODO with ID: ${id}
         Has a title of: ${title}
         Is it finished? ${finished}
     `)
-});
+};
